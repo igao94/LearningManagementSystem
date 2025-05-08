2901f7b550a45e103a70df0f3740a4c7634b0bf6
igao94 <igao998@gmail.com>
2025-05-08 09:37:20 +0200

    Rename email to emailSender for clarity

--- src/Infrastructure/Services/EmailServices/EmailSender.cs
+++ src/Infrastructure/Services/EmailServices/EmailSender.cs
@@ -4,14 +4,14 @@ using FluentEmail.Core;
 
 namespace Infrastructure.Services.EmailServices;
 
-public class EmailSender(IFluentEmail email) : IEmailSender
+public class EmailSender(IFluentEmail emailSender) : IEmailSender
 {
     public async Task SendConfirmationLinkAsync(User student, string verificationLink)
     {
-        await email
-           .To(student.Email)
-           .Subject("Email verification for LearningManagementSystem")
-           .Body($"To verify your email <a href='{verificationLink}'>click here</a>", isHtml: true)
-           .SendAsync();
+        await emailSender
+            .To(student.Email)
+            .Subject("Email verification for LearningManagementSystem")
+            .Body($"To verify your email <a href='{verificationLink}'>click here</a>", true)
+            .SendAsync();
     }
 }
